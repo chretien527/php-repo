1111e29121a70b6844bc3709287fd080082c4611
NAMBAJIMANA SANO Chretien <chretiensano@gmail.com>
2026-05-31 21:11:06 +0200

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,54 @@
+<?php
+
+include("db.php");
+
+$id = $_GET['id'];
+
+$query = "SELECT * FROM students WHERE id=$id";
+$result = mysqli_query($conn,$query);
+
+$row = mysqli_fetch_assoc($result);
+
+if(isset($_POST['update'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $course = $_POST['course'];
+
+    $updateQuery = "UPDATE students
+                    SET name = '$name',
+                        email = '$email',
+                        course = '$course'
+                    WHERE id=$id";
+
+
+mysqli_query($conn,$updateQuery);
+header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Edit Student</title>
+    </head>
+    <body>
+        <h1>Edit Student</h1>
+
+        <form method="POST">
+
+        <input type="text" name="name"
+        value="<?php echo $row['name']; ?>" required><br><br>
+
+        
+        <input type="email" name="email"
+        value="<?php echo $row['email']; ?>" required><br><br>
+
+
+        <input type="text" name="course"
+        value="<?php echo $row['course']; ?>"required><br><br>
+
+        <button type="submit" name="update">update</button>
+        </form>
+    </body>
+</html>
